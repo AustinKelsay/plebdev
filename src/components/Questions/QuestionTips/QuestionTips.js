5dc5e463b7a536520d5b783ff6a70a11d307ceab
austinkelsay <austinkelsay@yahoo.com>
2022-10-11 13:51:28 -0500

    QuestionTips fully working

--- src/components/Questions/QuestionTips/QuestionTips.js
+++ src/components/Questions/QuestionTips/QuestionTips.js
@@ -1,9 +1,36 @@
-import React from "react";
-import { Flex, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Flex, Text, Box } from "@chakra-ui/react";
 import { FaArrowUp, FaArrowDown } from "react-icons/fa";
+import { tipQuestion } from "../../../redux/questionsReducer";
+import { useDispatch, useSelector } from "react-redux";
+import { useRouter } from "next/router";
+import ClipLoader from "react-spinners/ClipLoader";
 import styles from "./styles.module.css";
 
 const QuestionTips = ({ score }) => {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const { slug } = router.query;
+
+  const [currentScore, setCurrentScore] = useState(score);
+
+  const loading = useSelector((state) => state.questions.loading);
+
+  const handleTip = (tip) => {
+    const requestObject = {
+      slug,
+      tip,
+      score,
+    };
+    dispatch(tipQuestion(requestObject))
+      .unwrap()
+      .then((res) => {
+        setCurrentScore(res.score);
+      })
+      .catch((rejectedValueOrSerializedError) => {
+        console.log(rejectedValueOrSerializedError);
+      });
+  };
   return (
     <Flex
       className={styles.votesContainer}
@@ -11,9 +38,13 @@ const QuestionTips = ({ score }) => {
       justifyContent={"space-evenly"}
       alignItems={"center"}
     >
-      <FaArrowUp cursor={"pointer"} />
-      <Text>{score}</Text>
-      <FaArrowDown cursor={"pointer"} />
+      <FaArrowUp onClick={() => handleTip(1)} cursor={"pointer"} />
+      {loading ? (
+        <ClipLoader size={17} color="orange" />
+      ) : (
+        <Text>{currentScore}</Text>
+      )}
+      <FaArrowDown onClick={() => handleTip(-1)} cursor={"pointer"} />
     </Flex>
   );
 };
--- src/redux/questionsReducer/index.js
+++ src/redux/questionsReducer/index.js
@@ -21,6 +21,21 @@ export const getQuestion = createAsyncThunk(
   }
 );
 
+export const tipQuestion = createAsyncThunk(
+  "questions/upvote",
+  async (params, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.put(
+        "http://localhost:3000/api/questions/" + params.slug,
+        { score: params.score + params.tip }
+      );
+      return data;
+    } catch (error) {
+      rejectWithValue(error.response);
+    }
+  }
+);
+
 export const questionsSlice = createSlice({
   name: "questions",
   initialState,
@@ -37,6 +52,20 @@ export const questionsSlice = createSlice({
     [getQuestion.rejected]: (state, action) => {
       state.loading = false;
       state.isSuccess = false;
+    },
+    [tipQuestion.pending]: (state, action) => {
+      state.loading = true;
+      console.log("loading");
+    },
+    [tipQuestion.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.isSuccess = true;
+      console.log("success");
+    },
+    [tipQuestion.rejected]: (state, action) => {
+      state.loading = false;
+      state.isSuccess = false;
+      console.log("rejected");
     },
   },
 });
